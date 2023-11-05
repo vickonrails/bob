8c1dac8e03eb061edd30d50e8fa816d4d8e6f74a
vickonrails <victorofoegbu0009@gmail.com>
2023-11-05 13:17:47 +0100

    chore: resume images

--- src/components/builder/layout.tsx
+++ src/components/builder/layout.tsx
@@ -8,7 +8,10 @@ export function Sidebar() {
     return (
         <aside className="w-1/2 py-8 max-h-screen overflow-y-auto bg-white print:hidden">
             <section className="max-w-xl m-auto">
-                <h1 className="text-xl mb-12 font-medium text-center">Resume Builder</h1>
+                <div className="flex justify-center gap-2 items-center mb-12">
+                    <img src='/logo.png' className="w-8 h-8" />
+                    <h1 className="text-xl font-medium text-center">Bob</h1>
+                </div>
                 <ResumeForm />
             </section>
         </aside>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,30 @@
 import BasicInformationDialog from "@/components/ui/basic-information-dialog"
 import { useDialog } from "@/hooks/useDialog"
-import { cn } from "@/lib/utils"
-import { TemplateType } from "./root"
-import { buttonVariants } from "@/components/ui/button-variants"
 import { useResumeTemplate } from "@/hooks/useResumeTemplate"
 import { useCallback } from "react"
+import { TemplateType } from "./root"
 
 function Index() {
     const { isOpen, openDialog, setOpen } = useDialog({})
-    const btnCTAClasses = cn(buttonVariants({ variant: "default" }), 'text-lg')
 
     return (
         <div className="bg-muted min-h-full">
             <div className="hero bg-white">
-                <header className="text-center py-28 flex flex-col gap-4 max-w-lg mx-auto items-center">
+                <header className="text-center py-20 flex flex-col gap-4 max-w-xl mx-auto items-center">
                     <h1 className="text-6xl/tight font-bold">Build simple and Effective Resumes</h1>
-                    <p className="text-xl text-muted-foreground">Resume is a free, safe, open source resume builder. No accounts required, no data collected, works well with Application Tracking Systems. </p>
-                    <a href='#templates' className={btnCTAClasses}>Select a Template</a>
+                    <p className="text-xl text-muted-foreground">Bob is a free, safe, open source resume builder. No accounts required, no data collected, works well with Application Tracking Systems. Select a template below to begin...</p>
                 </header>
             </div>
 
             <section className="py-11" id="templates">
-                <div className="h-96 max-w-7xl mx-auto">
+                <div className="max-w-7xl mx-auto">
                     <Templates openDialog={openDialog} />
                 </div>
             </section>
+
+            <footer className="py-4 text-muted-foreground text-center text-sm">
+                &copy; {new Date().getFullYear()} Bob. All rights reserved.
+            </footer>
 
             <BasicInformationDialog
                 open={isOpen}
@@ -45,13 +45,23 @@ function Templates({ openDialog }: { openDialog: () => void }) {
     // TODO: use the template from local storage
     // TODO: add other template pictures
     return (
-        <button onClick={() => handleSelect('plain')}>
-            <img
-                src="/resume-sample.png"
-                width={300}
-                className="cursor-pointer"
-            />
-        </button>
+        <div className="flex gap-4 justify-center">
+            <button onClick={() => handleSelect('plain')}>
+                <img
+                    src="/plain.png"
+                    width={400}
+                    className="cursor-pointer bg-contain"
+                />
+            </button>
+
+            <button onClick={() => handleSelect('mature')}>
+                <img
+                    src="/mature.png"
+                    width={400}
+                    className="cursor-pointer bg-cover"
+                />
+            </button>
+        </div>
     )
 }
 
